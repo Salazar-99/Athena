b86c85579b8b79240f1c38d42107b17e6a9b8a7e
Gerardo Salazar <Drifta10.gs@gmail.com>
2022-01-18 20:46:21 -0600

    Adding Flux bootstrap script and a test Pod for flux

---
+++ Flux/bootstrap.sh
@@ -0,0 +1,7 @@
+flux bootstrap github \
+  --components-extra=image-reflector-controller,image-automation-controller \
+  --owner=$GITHUB_USER \
+  --repository=Athena \
+  --path=./Flux \
+  --personal \
+  --branch=main
